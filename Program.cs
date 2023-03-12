cde1bec721a1c0dfca8b6416ee7020cfed5f8ef2
Carlos García Murillo <c.g.murillo91@gmail.com>
2023-03-12 16:57:16 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program_Ficheros
+{
+    static void Main(string[] args)
+    {
+        // Abre el archivo de texto para leer
+        StreamReader file = File.OpenText("tecleado.txt");
+
+        string linea;
+        int contador = 0;
+
+        // Lee las tres primeras líneas del archivo
+        while ((linea = file.ReadLine()) != null && contador < 3)
+        {
+            Console.WriteLine(linea);
+            contador++;
+        }
+
+        // Cierra el archivo
+        file.Close();
+
+        Console.WriteLine("Programa finalizado.");
+        Console.ReadLine();
+    }
+}
